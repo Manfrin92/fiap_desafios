7dcb4bffac72e43cd30ae556f62d3ceab6787b76
Manfrin92 <manfredd_10@hotmail.com>
2025-06-15 15:22:19 -0300

    feat: label and checkbox

--- desafio1/src/app/components/Video.tsx
+++ desafio1/src/app/components/Video.tsx
@@ -1,5 +1,7 @@
+import { Label } from "@/components/ui/label";
 import type { VideoResponse } from "../types/videoResponse";
 import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
 
 type VideoProps = {
   video: VideoResponse;
@@ -7,26 +9,18 @@ type VideoProps = {
 
 export function Video({ video }: VideoProps) {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        gap: "0.5rem",
-        padding: "0.5rem",
-        border: "1px white solid",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <input type="checkbox" style={{}} />
-      <Image src={video.thumbnailUrl} alt="YouTube Thumbnail" width={96} height={65} style={{ objectFit: "cover" }} />
-      <div style={{ display: "flex", flexDirection: "column", gap: "0.25rem" }}>
-        <span className="text-[0.85rem] font-bold">{video.name}</span>
-        <span className="text-[0.75rem]">
-          {video.description?.length > 105 ? video.description.slice(0, 105) + "..." : video.description}
-        </span>
-        <span className="text-[0.6rem]">Duration: {video.duration}</span>
-      </div>
+    <div className="flex flex-row gap-2 p-2 border-neutral-600 border-2 justify-center items-center">
+      <Checkbox id={video.name} />
+      <Label htmlFor={video.name} className="cursor-pointer">
+        <Image src={video.thumbnailUrl} alt="YouTube Thumbnail" width={96} height={65} style={{ objectFit: "cover" }} />
+        <div className="flex flex-col gap-1">
+          <span className="text-[0.85rem] font-bold">{video.name}</span>
+          <span className="text-[0.75rem]">
+            {video.description?.length > 105 ? video.description.slice(0, 105) + "..." : video.description}
+          </span>
+          <span className="text-[0.6rem]">Duration: {video.duration}</span>
+        </div>
+      </Label>
     </div>
   );
 }
